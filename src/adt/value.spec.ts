ee814fd6ba023fe0e628db1f6883cb9220af4516
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2025-10-14 08:47:50 +1100

    test: ADT.value, add target infer assertion

--- src/adt/value.spec.ts
+++ src/adt/value.spec.ts
@@ -1,3 +1,4 @@
+import type { ADT } from "../adt.js";
 import { Value } from "./value.js";
 
 import type { Expect, Equal } from "pb.expectequal";
@@ -12,5 +13,10 @@ test("Value", () => {
 		const value = Value("$type")("Foo", { data: "..." });
 		expect(value).toStrictEqual({ $type: "Foo", data: "..." });
 		!0 as Expect<Equal<typeof value, { $type: "Foo"; data: string }>>;
+	}
+	{
+		type Value = ADT<{ Foo: { a: string }; Bar: { b: string } }>;
+		const value: Value = Value("$type")("Foo", { a: "..." });
+		expect(value).toStrictEqual({ $type: "Foo", a: "..." });
 	}
 });
